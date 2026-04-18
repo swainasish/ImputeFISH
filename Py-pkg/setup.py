c834e94e8a7dffe68a548bd90e60a14a7da8e440
Asish Kumar Swain <swainkasish@gmail.com>
2026-04-18 11:30:35 +0530

    py-pkg

---
+++ Py-pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+   'Development Status :: 5 - Production/Stable',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'Operating System :: POSIX :: Linux',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='ImputeFISH',
+  version='0.0.3',
+  description='ImputeFISH: A Spatially Aware and Scalable Framework for Gene Imputation in Imaging-Based Spatial Transcriptomics',
+  long_description=open('README.md').read() ,
+  url='',  
+  author='Asish Kumar Swain',
+  author_email='swainkasish@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='willupdate', 
+  packages=find_packages(),
+  install_requires=["scikit-learn>=1.7.2",
+        "scipy>=1.15.3",
+        "seaborn>=0.13.2"]
+)
